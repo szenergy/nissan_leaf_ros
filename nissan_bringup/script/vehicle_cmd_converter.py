e5df93564d4a9a201ededc057c97a9f31ca32bb7
horverno <horverno@gmail.com>
2020-12-03 15:12:50 +0100

    Changes from the last MPC tuning test

--- nissan_bringup/script/vehicle_cmd_converter.py
+++ nissan_bringup/script/vehicle_cmd_converter.py
@@ -33,7 +33,7 @@ def ctrCallBack(msg):
     global pub_cmd
     cmd_msg = szelec_msg.VehicleVelocityControlCommand()
     cmd_msg.ref_vehicle_speed = msg.cmd.linear_velocity # km/h
-    cmd_msg.ref_steer_angle = msg.cmd.steering_angle
+    cmd_msg.ref_steer_angle = -msg.cmd.steering_angle
     cmd_msg.set_autonomous_mode = True
     cmd_msg.header.stamp = rospy.Time.now()
     #print(cmd_msg)
